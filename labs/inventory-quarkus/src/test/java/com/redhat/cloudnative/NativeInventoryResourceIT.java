28d0b0c2f08fd9f8b38fcbe78d8991c6f4515900
mcouliba <mcouliba@redhat.com>
2019-10-22 11:37:43 +0200

    Updated Developer part

---
+++ labs/inventory-quarkus/src/test/java/com/redhat/cloudnative/NativeInventoryResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.cloudnative;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeInventoryResourceIT extends InventoryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
