4fad37de223558a5630b25cf60f41f0531fb6956
mcouliba <mcouliba@redhat.com>
2019-11-15 15:00:41 +0100

    Update coolstore.config.js

--- labs/web-nodejs/config/coolstore.config.js
+++ labs/web-nodejs/config/coolstore.config.js
@@ -1,6 +1,6 @@
 var config = {
-    API_ENDPOINT: 'gateway-' + process.env.OPENSHIFT_BUILD_NAMESPACE,
-    SECURE_API_ENDPOINT: 'secure-gateway-' + process.env.SECURE_COOLSTORE_GW_SERVICE,
+    API_ENDPOINT: 'gateway-coolstore-' + process.env.OPENSHIFT_BUILD_NAMESPACE,
+    SECURE_API_ENDPOINT: 'secure-gateway-coolstore-' + process.env.SECURE_COOLSTORE_GW_SERVICE,
     SSO_ENABLED: process.env.SSO_URL ? true : false
 };
 
