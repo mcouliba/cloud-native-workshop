7c0d2fa39d632485b041bb1396ee1eb343c1b226
mcouliba <mcouliba@redhat.com>
2020-04-07 22:16:01 +0200

    Added Istio Configuration

--- labs/web-nodejs/app/coolstore.config.js
+++ labs/web-nodejs/app/coolstore.config.js
@@ -1,2 +1,2 @@
 angular.module("app")
-.constant("COOLSTORE_CONFIG", {"API_ENDPOINT":"http://gateway-cn-project1.apps.cluster-nantes-f5af.nantes-f5af.example.opentlc.com/","SECURE_API_ENDPOINT":"secure-gateway-undefined","SSO_ENABLED":false});
+.constant("COOLSTORE_CONFIG", {"API_ENDPOINT":"gateway-coolstore-undefined","SECURE_API_ENDPOINT":"secure-gateway-coolstore-undefined","SSO_ENABLED":false});
