98444f2d7eca9922b491bdc8e18fdde31e1db03b
mcouliba <mcouliba@redhat.com>
2020-07-20 15:23:13 +0200

    Updated Solutions scripts

--- labs/solutions/deploy_coolstore.sh
+++ labs/solutions/deploy_coolstore.sh
@@ -8,3 +8,5 @@ $DIRECTORY/catalog-spring-boot/solve_deploy.sh
 $DIRECTORY/inventory-quarkus/solve_deploy.sh
 $DIRECTORY/gateway-vertx/solve_deploy.sh
 $DIRECTORY/web-nodejs/deploy.sh
+$DIRECTORY/health-probes/deploy.sh
+$DIRECTORY/app-config/deploy.sh
