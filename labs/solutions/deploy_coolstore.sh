3159196a5c124f761e80cc11339e3e4175d27064
mcouliba <mcouliba@redhat.com>
2020-07-20 15:41:22 +0200

    Deleted all before deploying

--- labs/solutions/deploy_coolstore.sh
+++ labs/solutions/deploy_coolstore.sh
@@ -4,6 +4,10 @@
 
 DIRECTORY=`dirname $0`
 
+#Starting from scratch
+oc delete project my-project${CHE_WORKSPACE_NAMESPACE#user}
+oc new-project my-project${CHE_WORKSPACE_NAMESPACE#user}
+
 $DIRECTORY/catalog-spring-boot/solve_deploy.sh
 $DIRECTORY/inventory-quarkus/solve_deploy.sh
 $DIRECTORY/gateway-vertx/solve_deploy.sh
